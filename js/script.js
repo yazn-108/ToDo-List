7db95aa6dc28e6de4c44b2e43f036963abdf8158
yazn_108 <102805404+yazn-108@users.noreply.github.com>
2023-07-30 20:28:50 +0300

    ToDo List

--- js/script.js
+++ js/script.js
@@ -33,6 +33,7 @@ addButton.addEventListener("click", (e) =>{
             let span = document.createElement("span");
             span.innerHTML = input.value;
             span.className = "task-text";
+            span.setAttribute("onclick", "completed(this);")
             task.appendChild(span);
             let deleteButton = document.createElement("button");
             deleteButton.className = "delete";
@@ -43,6 +44,12 @@ addButton.addEventListener("click", (e) =>{
             setTimeout(()=>{
                 addButton.style = `transform: rotate(0deg);`;
             },300);
+            let storedTasks = localStorage.getItem("tasks");
+            let tasksArray = JSON.parse(storedTasks) || [];
+            let newTask = {task: input.value,completed: false};
+            tasksArray.push(newTask);
+            let arrayToJson = JSON.stringify(tasksArray);
+            localStorage.setItem("tasks",arrayToJson);
             input.value = "";
             tasksCount.innerHTML = tasks.childElementCount;
         };
@@ -60,6 +67,12 @@ addButton.addEventListener("click", (e) =>{
 document.addEventListener("click",(e) =>{
     if(e.target.className == "delete"){
         e.target.parentNode.remove();
+        let taskValue = e.target.parentNode.querySelector('.task-text').innerText;
+        let tasksArray = JSON.parse(localStorage.getItem("tasks"));
+        let taskIndex = tasksArray.findIndex(task => task.task === taskValue);
+        tasksArray.splice(taskIndex, 1);
+        let updatedTasks = JSON.stringify(tasksArray);
+        localStorage.setItem("tasks", updatedTasks);
         tasksCount.innerHTML = tasks.childElementCount;
         if(tasks.childElementCount === 0){
             tasks.appendChild(noTasks);
@@ -76,6 +89,7 @@ deleteTasks.addEventListener("click",() => {
     let allTasks = document.querySelectorAll(".tasks .task-box");
     allTasks.forEach(function(task) {
         task.remove();
+        localStorage.clear();
         tasksCount.innerHTML = tasks.childElementCount;
     });
     if(tasks.childElementCount === 0){
@@ -85,11 +99,46 @@ deleteTasks.addEventListener("click",() => {
 let deleteCompleted = document.querySelector(".delete-completed");
 deleteCompleted.addEventListener("click",() => {
     let allCompleted = document.querySelectorAll(".tasks .task-box .finished");
-    allCompleted.forEach((e)=>{
+    allCompleted.forEach((e,i)=>{
         e.parentElement.remove();
         tasksCount.innerHTML = tasks.childElementCount;
         if(tasks.childElementCount === 0){
             tasks.appendChild(noTasks);
         };
     });
+});
+if(localStorage.getItem("tasks") !== null){
+let storedTasks = JSON.parse(localStorage.getItem("tasks"))
+storedTasks.forEach((e) =>{
+    noTasks.remove();
+    let task = document.createElement("div");
+    task.className = "task-box";
+    let span = document.createElement("span");
+    span.innerHTML = e.task;
+    span.className = "task-text";
+    span.setAttribute("onclick", "completed(this);")
+    task.appendChild(span);
+    let deleteButton = document.createElement("button");
+    deleteButton.className = "delete";
+    deleteButton.textContent = "Delete";
+    task.appendChild(deleteButton);
+    tasks.appendChild(task);
+    tasksCount.innerHTML = tasks.childElementCount;
+    tasksCompleted.innerHTML = 
+    document.querySelectorAll(".tasks .task-box .finished").length;
+});};
+function completed(e) {
+    let items = document.querySelectorAll('.task-text');
+    let index = Array.prototype.indexOf.call(items, e)
+let storedArray = Array.from(JSON.parse(localStorage.getItem('tasks')))
+storedArray[index].completed = !storedArray[index].completed
+localStorage.setItem("tasks", JSON.stringify(storedArray));
+};
+let items = document.querySelectorAll('.task-text');
+items.forEach((e,i) => {
+    let storedArray = JSON.parse(localStorage.getItem('tasks'));
+    if(storedArray[i].completed === true){
+        e.classList.add("finished");
+        tasksCompleted.innerHTML = document.querySelectorAll(".tasks .task-box .finished").length;
+    };
 });
